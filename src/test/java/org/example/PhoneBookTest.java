8c54c0a26ca0144acc1b14f3a47cffcfc60b2efe
Svetlana Andros <lana-444@mail.ru>
2024-08-01 19:53:14 +0300

    Add implemented

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,14 +1,15 @@
 package org.example;
 
 import org.junit.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PhoneBookTest {
+
     @Test
     public void testAdd() {
         PhoneBook phoneBook = new PhoneBook();
-        int result  = phoneBook.add("Liza", 89181230990);
+        int result  = phoneBook.add("Liza", "89181231234");
         assertEquals(1, result);
     }
 }
